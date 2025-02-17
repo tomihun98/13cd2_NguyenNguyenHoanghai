f6c739fa377b87ee7cf43f1c118069a89795b06e
tomihun98 <tamastomi98@gmail.com>
2025-02-17 14:56:24 +0100

    insert elkészitése

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const bodyparser = require("body-parser");
 const { request } = require("http");
+const { error } = require("console");
 app.use(cors());
 app.use(bodyparser.json());
 
@@ -12,18 +13,54 @@ const db = mysql.createConnection(
         user: "root",
         host: "localhost",
         port: 3307,
-        password: "root",
+        password: "",
         database: "teliolimpia"
     }
 )
 
 // a szerver futásának ellenőrzése
 
-app.get("/", (req,res)  => {
+app.get("/", (req, res) => {
     res.send("A szerver működik !")
 }
 
 )
+
+app.get("/v", (req, res) => {
+
+    const sql = "SELECT * FROM `versenyzok`";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+
+    )
+
+}
+)
+
+app.get("/v6", (req, res) => {
+
+    const sql = "SELECT * FROM `versenyzok` WHERE ID = 6;";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+    )
+}
+)
+
+app.post("/vuj", (req, res) => {
+    const sql = "INSERT INTO 'versenyzok' ('ID','versenyzo') VALUES (?,?)";
+    const values = [req.body.ID, req.body.versenyzo];
+    db.query(sql, (err, result) => {
+        if (err) return res.status(500).json({error: "hibás adatbázis művelet !"});
+        return res.json(result);
+    }
+    )
+}
+)
+
 
 app.listen(3000, () => {
     console.log('A szerver a 3000 porton fut!')
