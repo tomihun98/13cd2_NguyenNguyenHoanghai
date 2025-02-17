56aacc25d554b7e4f8ff50d41e258669fadf82ba
tomihun98 <tamastomi98@gmail.com>
2025-02-17 14:14:30 +0100

    működik a szerver beállítása
    mysql szerver kapcsolatának létrehozása ellenőrző get megírása

--- server.js
+++ server.js
@@ -3,11 +3,29 @@ const mysql = require("mysql");
 const app = express();
 const cors = require("cors");
 const bodyparser = require("body-parser");
+const { request } = require("http");
 app.use(cors());
 app.use(bodyparser.json());
 
+const db = mysql.createConnection(
+    {
+        user: "root",
+        host: "localhost",
+        port: 3307,
+        password: "root",
+        database: "teliolimpia"
+    }
+)
+
+// a szerver futásának ellenőrzése
+
+app.get("/", (req,res)  => {
+    res.send("A szerver működik !")
+}
+
+)
 
 app.listen(3000, () => {
-    console.log('A szerver a 3000 porton futR')
+    console.log('A szerver a 3000 porton fut!')
 })
 
